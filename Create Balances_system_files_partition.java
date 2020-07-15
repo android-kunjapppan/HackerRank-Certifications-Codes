dec82fbc8aa2d067178fda102b44653c3df4f508
Mohd Maheer <64364736+android-kunjapppan@users.noreply.github.com>
2020-07-16 03:48:46 +0530

    Create Create Balances_system_files_partition.java

---
+++ Create Balances_system_files_partition.java
@@ -0,0 +1,92 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+class Result {
+
+    /*
+     * Complete the 'mostBalancedPartition' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY parent
+     *  2. INTEGER_ARRAY files_size
+     */
+
+    public static int mostBalancedPartition(List<Integer> parent, List<Integer> files_size) {
+        DirectedGraph graph = new DirectedGraph();
+        for (int index = 0 ; index < parent.size() ; index++) {
+            graph.addVertex(index, files_size.get(index));
+            if (index != 0) {
+                graph.addEdge(parent.get(index), index);
+            }
+        }
+        return graph.mostBalancedPartition();
+    }
+
+    private static class DirectedGraph {
+        Map<Integer, Vertex> vertices = new HashMap<>();
+        private long diskSize;
+
+        private static class Vertex {
+            private final int fileSize;
+            private long totalMemoryRequired;
+            Set<Vertex> edges = new HashSet<>();
+
+            Vertex(int fileSize) {
+                this.fileSize = fileSize;
+            }
+
+            void addEdge(Vertex to) {
+                this.edges.add(to);
+            }
+        }
+
+        public void addVertex(int data, int fileSize) {
+            vertices.put(data, new Vertex(fileSize));
+        }
+
+        public void addEdge(int from, int to) {
+            Vertex fromVertex = vertices.get(from);
+            fromVertex.addEdge(vertices.get(to));
+        }
+
+        int mostBalancedPartition() {
+            computeFileDataDistribution();
+            return mostBalancedPartition(vertices.get(0));
+        }
+
+        private void computeFileDataDistribution() {
+            diskSize = computeFileDataDistribution(vertices.get(0));
+        }
+
+        private long computeFileDataDistribution(Vertex current) {
+            long totalMemoryRequired = current.fileSize;
+            for (Vertex child : current.edges) {
+                totalMemoryRequired += computeFileDataDistribution(child);
+            }
+            current.totalMemoryRequired = totalMemoryRequired;
+            return current.totalMemoryRequired;
+        }
+
+        private int mostBalancedPartition(Vertex current) {
+            int result = Integer.MAX_VALUE;
+            for (Vertex child : current.edges) {
+                result = Math.min(result, mostBalancedPartition(child));
+                result = (int) Math.min(result, Math.abs(diskSize - 2 * child.totalMemoryRequired));
+            }
+            return result;
+        }
+    }
+    // Write your code here
+
+    
+
+}
+
+public class Solution {
